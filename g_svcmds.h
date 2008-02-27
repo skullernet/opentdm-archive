f136f08c8354471b7cc2f5fe2bea09b14a69a396
R1CH <r1ch@r1ch.net>
2008-02-27 03:09:24 +0000

    Add missing header.
    git-svn-id: svn://svn.quakedev.com/opentdm/trunk@69 ce06de39-ea2a-0410-99ec-b600a5f3c494

---
+++ g_svcmds.h
@@ -0,0 +1,18 @@
+typedef struct
+{
+	unsigned	mask;
+	unsigned	compare;
+	unsigned	expire;
+} ipfilter_t;
+
+#define	MAX_IPFILTERS	1024
+
+ipfilter_t	ipfilters[MAX_IPFILTERS];
+unsigned	numipfilters;
+
+void SVCmd_AddIP_f (edict_t *ent, char *ip, int exp);
+void SVCmd_RemoveIP_f (edict_t *ent, char *ip);
+void SVCmd_ListIP_f (edict_t *ent);
+qboolean StringToFilter (char *s, ipfilter_t *f, int exp);
+qboolean CheckIpFilterList (edict_t *ent, unsigned *i);
+
