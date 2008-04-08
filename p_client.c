512e9a1a2c80822e35d1385e337e065eaa18102a
R1CH <r1ch@r1ch.net>
2008-04-08 08:08:27 +0000

    Fix compile error, oops.
    git-svn-id: svn://svn.quakedev.com/opentdm/trunk@99 ce06de39-ea2a-0410-99ec-b600a5f3c494

--- p_client.c
+++ p_client.c
@@ -759,7 +759,7 @@ edict_t *SelectDeathmatchSpawnPoint (void)
 	//in the first 1 second of a match start, or the first 5 seconds of warmup, avoid telefrags above
 	//all other conditions
 	if ((tdm_match_status >= MM_PLAYING && level.framenum - level.match_start_framenum < SECS_TO_FRAMES (1)) ||
-		(tdm_match_status == MM_WARMUP && level.framenum - level.warmup_start_framenum < SECS_TO_FRAMES (5))
+		(tdm_match_status == MM_WARMUP && level.framenum - level.warmup_start_framenum < SECS_TO_FRAMES (5)))
 		return SelectRandomDeathmatchSpawnPointAvoidingTelefrag ();
 
 	if ( (int)(dmflags->value) & DF_SPAWN_FARTHEST)
