0ddeaccef193b0b119da87193d02cf7ca139f30d
R1CH <r1ch@r1ch.net>
2009-04-14 22:16:54 +0000

    Fix missing linux header.
    git-svn-id: svn://svn.quakedev.com/opentdm/trunk@171 ce06de39-ea2a-0410-99ec-b600a5f3c494

--- g_tdm_curl.c
+++ g_tdm_curl.c
@@ -37,6 +37,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <netdb.h>
 #endif
 
 typedef struct dlhandle_s
